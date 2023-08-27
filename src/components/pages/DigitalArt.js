8f33055129c7a9ae1371c87084dc774fff393240
christinaqiu3 <christinaqiu3@gmail.com>
2023-08-27 02:42:47 -0400

    updated dig-art

--- src/components/pages/DigitalArt.js
+++ src/components/pages/DigitalArt.js
@@ -17,36 +17,139 @@ export default function DigitalArt() {
                         alt="Grandparents"
                         className="profile-image"
                     />
+
+                </div>
+                <div className="text-container">
+                    <h1 className="fade-in-text">Grandparents
+                    </h1>
+                    <h2 className="fade-in-text">
+                        A drawing I made for my grandparents for Christmas.
+                    </h2>
+                </div>
+
+
+
+
+                <div className="image-container">
                     <img
                         src="images/chibi.jpeg"
                         alt="Chibi"
                         className="profile-image"
                     />
+
+                </div>
+                <div className="text-container">
+                    <h1 className="fade-in-text">Chibi Me
+                    </h1>
+                    <h2 className="fade-in-text">
+                        A stylized drawing of me inspired by Hizgi's artstyle.
+                    </h2>
+                </div>
+
+
+
+
+
+
+
+                <div className="image-container">
                     <img
                         src="images/chrollo.jpeg"
                         alt="Chrollo"
                         className="profile-image"
                     />
+
+                </div>
+                <div className="text-container">
+                    <h1 className="fade-in-text">Chrollo
+                    </h1>
+                    <h2 className="fade-in-text">
+                        Fanart of a character from Hunter X Hunter.
+                    </h2>
+                </div>
+
+
+
+
+
+
+                <div className="image-container">
                     <img
                         src="images/red.jpeg"
                         alt="Red"
                         className="profile-image"
                     />
+
+                </div>
+                <div className="text-container">
+                    <h1 className="fade-in-text">Red
+                    </h1>
+                    <h2 className="fade-in-text">
+                        An original character I made inspired by Little Red Riding Hood.
+                    </h2>
+                </div>
+
+
+
+
+                <div className="image-container">
                     <img
-                        src="images/sun3.png"
+                        src="images/sun.png"
                         alt="Sun"
                         className="profile-image"
                     />
+
+                </div>
+                <div className="text-container">
+                    <h1 className="fade-in-text">Sun
+                    </h1>
+                    <h2 className="fade-in-text">
+                        A study of a selfie I took with cool lighting.
+                    </h2>
+                </div>
+
+
+
+
+
+                <div className="image-container">
                     <img
                         src="images/squidbee.jpeg"
                         alt="Friend"
                         className="profile-image"
                     />
+
+                </div>
+                <div className="text-container">
+                    <h1 className="fade-in-text">Friend
+                    </h1>
+                    <h2 className="fade-in-text">
+                        A drawing I made for a friend's birthday.
+                        She always wears a wool sweatshirt so she reminds me of a sheep.
+                        She loves nature and bees.
+                    </h2>
+                </div>
+
+
+                <div className="image-container">
                     <img
                         src="images/surfer.jpeg"
                         alt="Surfer"
                         className="profile-image"
                     />
+
+                </div>
+                <div className="text-container">
+                    <h1 className="fade-in-text">Surfer
+                    </h1>
+                    <h2 className="fade-in-text">
+                        I like the shading in the hair. I had fun making the highlights.
+                    </h2>
+                </div>
+
+
+
+                <div className="image-container">
                     <img
                         src="images/straw.jpeg"
                         alt="Strawberry"
@@ -55,22 +158,16 @@ export default function DigitalArt() {
 
                 </div>
                 <div className="text-container">
-                    <h1 className="fade-in-text">grandparents
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        <br></br><br></br>
-                        chibi
+                    <h1 className="fade-in-text">Strawberry Lemonade
                     </h1>
                     <h2 className="fade-in-text">
-                        I am currently studying Digital Media Design/Computer Science
-                        at the University of Pennsylvania.
+                        I like this slightly muted color palette.
                     </h2>
                 </div>
+
+
+
+
             </div>
             <Footer />
 
