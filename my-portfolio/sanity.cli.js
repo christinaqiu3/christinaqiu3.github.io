758184cf232fd643f2828a8442e809335ed20ba0
christinaqiu3 <christinaqiu3@gmail.com>
2024-06-21 00:51:07 -0400

    trying sanity backend

---
+++ my-portfolio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ac80qgh3',
+    dataset: 'production'
+  }
+})
---
+++ my-portfolio/schemaTypes/eventType.js
@@ -0,0 +1,35 @@
+export const eventType =[ {
+    name: 'Project',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            type: 'string',
+        },
+        {
+            name: 'video',
+            type: 'file',
+        },
+        {
+            name: 'skills',
+            type: 'array',
+            of: [{type: 'string'}],
+        },
+        {
+            name: 'link_read',
+            type: 'string',
+        },
+        {
+            name: 'link_link',
+            type: 'string',
+        },
+        {
+            name: 'link_code',
+            type: 'string',
+        },
+    ],
+}]
